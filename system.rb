7dc19449f3d4adf1a5e0c674ca9b6dc1c84ef4f6
codonculous <feidameng01@gmail.com>
2017-04-01 16:41:17 -0400

    create class Body, Planet, Star, Moon

--- system.rb
+++ system.rb
@@ -4,6 +4,9 @@ class System
 
   def self.system_create
     @@system.push(System.new)
+  end
+
+  def self.system_total_mass
 
   end
 
@@ -12,17 +15,54 @@ class System
     @argument = argument
   end
 
-
   @bodies = []
 
   def body_create
     @@bodies.push(Body.new(name,mass))
-
   end
 
   def total_mass
     @bodies.map(&:  ).sum
+  end
 
+end
+
+class Body
+
+  attr_reader :name, :mass
+
+  def initialize(name, mass)
+    @name = name
+    @mass = mass
+
+  end
+
+end
+
+class Planet < Body
+
+  def initialize(name,mass,day,year)
+    super(name,mass)
+    @day  = day
+    @year = year
+  end
+
+end
+
+class Star < Body
+  def initialize(name,mass,type)
+    super(name,mass,type)
+    @type = type
+
+  end
+
+end
+
+class Moon < Body
+
+  def initialize(name,mass,month)
+    @month = month
+    @planet = planet
   end
 
 
